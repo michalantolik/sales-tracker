27d872d8f81bf6f23405fe2b18558662fb718bd2
michalantolik <michal.antolik@gmail.com>
2023-10-22 12:54:54 +0200

    Configure local SQL Server database (DbContext)

---
+++ SalesTracker/Persistence/DatabaseService.cs
@@ -0,0 +1,23 @@
+﻿using Application.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Persistence
+{
+    public class DatabaseService :DbContext, IDatabaseService
+    {
+        private readonly IConfiguration configuration;
+
+        public DatabaseService(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = this.configuration.GetConnectionString("SalesTracker");
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
